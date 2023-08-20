367e91fa70a93387e6dbb77c6c1cad98ac77737d
mogg <m1664907189@gmail.com>
2023-08-20 16:53:50 +0800

    fix: pb pilicy

--- src/test-enum/xmake.lua
+++ src/test-enum/xmake.lua
@@ -3,12 +3,14 @@ target("pb3-v1")
     add_packages("protobuf-cpp", {public = true})
     add_rules("protobuf.cpp")
     add_files("pb3/v1/*.proto", {proto_public = true})
+    set_policy("build.across_targets_in_parallel", false)
 
 target("pb3-v2")
     set_kind("static")
     add_packages("protobuf-cpp", {public = true})
     add_files("pb3/v2/*.proto", {proto_public = true})
     add_rules("protobuf.cpp")
+    set_policy("build.across_targets_in_parallel", false) -- https://github.com/xmake-io/xmake/issues/4080
 
 target("gateway")
     set_kind("binary")
